f34807836c3f74e3f1c4db61791cbadbb9908485
Evgeni Sakov <sakov_00@inbox.ru>
2024-05-15 23:15:03 +0300

    Add scripts

---
+++ Assets/Assets/_Project/Scripts/ScriptableObjects/AbilitiesData/AccelerationAbilityData.cs
@@ -0,0 +1,11 @@
+﻿using Assets._Project.Scripts.ScriptableObjects.AbilitiesData.Abstract;
+using UnityEngine;
+
+namespace Assets._Project.Scripts.ScriptableObjects.AbilitiesData
+{
+    [CreateAssetMenu(fileName = "AccelerationAbilityData", menuName = "AbilityConfigs/AccelerationAbilityData")]
+    public class AccelerationAbilityData : ProlongedAbilityData
+    {
+        public float ValueSpeedUp;
+    }
+}
